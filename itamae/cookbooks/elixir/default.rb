258aee2dadd6a2ac4c54edc1a38deac3267c5481
j_kuriyama <junichi.kuriyama@supership.jp>
2016-01-05 18:44:06 +0900

    erlang,elixirを最新にしてインストール方法を変更

--- itamae/cookbooks/elixir/default.rb
+++ itamae/cookbooks/elixir/default.rb
@@ -5,27 +5,50 @@ node.reverse_merge!(
   }
 )
 
+node.reverse_merge!(
+  elixir: {
+    zip: 'https://github.com/elixir-lang/elixir/releases/download/v1.2.0/Precompiled.zip',
+    zip_file: 'Precompiled.zip'
+  }
+)
+
 package 'libssl-dev' do
   action :install
 end
 
-execute 'Add Erlang Solutions repo' do
-  #cwd node[:common][:config][:file_cache_path]
+#execute 'Add Erlang Solutions repo' do
+#  #cwd node[:common][:config][:file_cache_path]
+#  cwd '/tmp'
+#  command "wget -O #{node[:elixir][:repo_file]} #{node[:elixir][:repo]}"
+#  not_if "test -e ./#{node[:elixir][:repo_file]}"
+#end
+#
+#execute 'Add erlang-solutions' do
+#  #cwd node[:common][:config][:file_cache_path]
+#  cwd '/tmp'
+#  command " \
+#    dpkg -i #{node[:elixir][:repo_file]} && \
+#    apt-get update
+#  "
+#  not_if 'which iex'
+#end
+#
+#package 'elixir' do
+#  action :install
+#end
+
+execute 'Download elixir' do
   cwd '/tmp'
-  command "wget -O #{node[:elixir][:repo_file]} #{node[:elixir][:repo]}"
-  not_if "test -e ./#{node[:elixir][:repo_file]}"
+  command "wget #{node[:elixir][:zip]}"
+  not_if "test -e ./#{node[:elixir][:zip_file]}"
 end
 
-execute 'Add erlang-solutions' do
-  #cwd node[:common][:config][:file_cache_path]
+execute 'unzip elixir and setup path' do
   cwd '/tmp'
-  command " \
-    dpkg -i #{node[:elixir][:repo_file]} && \
-    apt-get update
+  command "\
+    sudo unzip #{node[:elixir][:zip_file]} -d /opt/elixir && \
+    echo 'export PATH=/opt/elixir/bin:$PATH' >> ~/.zshrc \
+    echo 'export PATH=/opt/elixir/bin:$PATH' >> ~/.bashrc \
   "
   not_if 'which iex'
-end
-
-package 'elixir' do
-  action :install
 end
--- itamae/cookbooks/erlang/default.rb
+++ itamae/cookbooks/erlang/default.rb
@@ -1,11 +1,18 @@
 node.reverse_merge!(
   erlang: {
-    source: 'http://www.erlang.org/download/otp_src_18.0.tar.gz', 
-    source_name: 'otp_src_18.0'
+    #source: 'http://www.erlang.org/download/otp_src_18.0.tar.gz', 
+    source: 'http://www.erlang.org/download/otp_src_18.2.tar.gz',
+    source_name: 'otp_src_18.2'
   }
 )
 
-%w(libncurses5-dev).each do |name|
+%w(libncurses5-dev libssl-dev build-essential libncurses-dev erlang-jinterface erlang-odbc plplot12-driver-wxwidgets).each do |name|
+  package name do
+    action :install
+  end
+end
+
+%w(openjdk-7-jdk m4 unixODBC fop unixodbc-dev libwxbase2.8 libwxgtk2.8-dev libqt4-opengl-dev libgtk2.0-dev xsltproc).each do |name|
   package name do
     action :install
   end
@@ -24,6 +31,7 @@ execute 'Install erlang' do
   command " \
     tar xzf #{node[:erlang][:source_name]}.tar.gz && \
     cd #{node[:erlang][:source_name]} && \
+    touch lib/wx/SKIP && \
     ./configure && make && \
     make install
   "
