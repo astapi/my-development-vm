b747e8d0761daa4e88894572bbeab7dc604b3a0a
astapi <jyunichi.k@gmail.com>
2016-10-11 13:13:34 +0900

    add rabbitmq

--- itamae/cookbooks/rabbitmq/default.rb
+++ itamae/cookbooks/rabbitmq/default.rb
@@ -4,6 +4,7 @@ execute 'add packaege' do
     echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list && \
     wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
   "
+  not_if 'which rabbitmqctl'
 end
 
 execute 'install rabbitmq' do
@@ -12,4 +13,19 @@ execute 'install rabbitmq' do
     sudo apt-get update && \
     sudo apt-get -y install rabbitmq-server
   "
+  not_if 'which rabbitmqctl'
+end
+
+template '/etc/default/rabbitmq-server' do
+  action :create
+  user 'root'
+  source 'templates/rabbitmq-server.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+service 'rabbitmq-server' do
+  action :restart
+  user 'root'
 end
