be82c3981f36981e3862f8d4d2a1d0e0e70afe5b
astapi <jyunichi.k@gmail.com>
2016-11-11 15:51:55 +0900

    Update default.rb

--- itamae/cookbooks/peco/default.rb
+++ itamae/cookbooks/peco/default.rb
@@ -1,6 +1,6 @@
 execute 'Download peco' do
   cwd '/tmp'
-  command "wget -O https://github.com/peco/peco/releases/download/v0.3.6/peco_linux_amd64.tar.gz"
+  command "wget https://github.com/peco/peco/releases/download/v0.3.6/peco_linux_amd64.tar.gz"
   not_if 'which peco'
 end
 
@@ -8,6 +8,6 @@ execute 'install' do
   cwd '/tmp'
   command "tar zxvf peco_linux_amd64.tar.gz \
     && cd peco_linux_amd64 \
-    && cp peco /usr/loca/bin"
+    && cp peco /usr/local/bin"
   not_if 'which peco'
 end
